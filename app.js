ae9c4f70e42cdb616c1b9632c56c4b3eca3613ce
angelfabiandagata-ui <angelfabiandagata@gmail.com>
2026-05-19 09:40:12 -0300

    configuracion del servidor puerto 300 y del .evn

---
+++ app.js
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+import express from 'express';
+import pug from 'pug';
+
+// CONSTANTES
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+//cargar pug
+app.set("view engine", "pug");
+app.set('views', './views');
+
+// MIDDLEWARES
+app.use(express.static('public'));
+
+// rutas
+app.get("/",(req, res , next)=>{
+    res.render("index");
+})
+
+
+// iniciar servidor
+app.listen(PORT, (err) => {
+      if (err) {
+        console.error(" [X] Error al iniciar el servidor: ", err);
+      }
+      console.log(` [✓] Servidor corriendo en el puerto:${PORT}`);
+    });
